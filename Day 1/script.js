8281a142068b9131f5839e5621ba253a8a822956
Saurabh-Chede <saurabhchede21@gmail.com>
2025-07-07 11:50:59 +0530

    day-1

---
+++ Day 1/script.js
@@ -0,0 +1,18 @@
+const run = ['kgf','ed','erdf','455']
+
+const result = run.map((num)=> {
+     +num + '1'
+})
+
+// console.log(result);
+
+const sum = run.forEach((item)=>{
+    console.log(`${item}  gold`);
+    
+    
+})
+
+console.log(sum);
+
+
+
