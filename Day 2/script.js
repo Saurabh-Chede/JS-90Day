53c3578cc2b04aae5393fd445a5abacb9844a845
Saurabh-Chede <saurabhchede21@gmail.com>
2025-07-08 21:15:22 +0530

    Day 2

--- Day 2/script.js
+++ Day 2/script.js
@@ -21,7 +21,7 @@
 function orderPizza() {
     return new Promise((resolve, reject) => {
         setTimeout(()=>{
-            const isDelivered = true
+            const isDelivered = 0
             if(isDelivered){
                 resolve("🍕 Pizza delivered!")
             }
@@ -50,6 +50,66 @@ async function eatDinner () {
     
 }
 
-eatDinner()
+// eatDinner()
+
+async function getUser () {
+    console.log('waiting...');
+
+    try{
+        const pizza = await fetch("https://jsonplaceholder.typicode.com/users/1")
+        const data = await pizza.json()
+        console.log(data);
+        
+    }
+    catch(err){
+        console.log(err);
+        console.log('arey kidhar hai data',err);
+        
+        
+    }
+    
+}
+
+// getUser()
+
+ function renderingUI () {
+    const dataCollection = setTimeout(()=>{
+
+        const nameData = {
+            user:'SAURABH',
+            age : 27
+        }
+
+        const result = console.log(nameData.age);
+        console.log(nameData);
+        console.table(nameData)
+        
+        
+
+    },5000)
+}
+
+// renderingUI()
+
+async function fetchUsers() {
+  const res = await fetch("https://jsonplaceholder.typicode.com/users");
+  const data = await res.json();
+
+  console.log("✅ Full User Data:");
+  console.table(data); // shows array of objects as table
+
+  console.log("📌 Specific Columns:");
+  console.table(data.map((user) => ({
+    ID: user.id,
+    Name: user.name,
+    Email: user.email,
+    City: user.address.city
+  })));
+}
+
+// fetchUsers();
+
+
+
 
 
